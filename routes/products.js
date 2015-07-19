a2b6f33ee7c461bad04be2fde7b7d2252960e76c
qinxuebing <qinxuebing_2002@google.com>
2015-07-19 11:39:01 +0800

    remove useless dependency

--- routes/products.js
+++ routes/products.js
@@ -8,12 +8,9 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-    console.log('0000000000000000');
     loadDress(function(docs){
-        console.log('555555555555',docs);
         res.json(docs);
     });
-    console.log('444444444444444444');
 });
 
 module.exports = router;
